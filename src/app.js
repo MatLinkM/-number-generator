9fde3845a2e18b408ad2d94c435837bc4607c317
Matheus Medeiros <matheuslinkm@gmail.com>
2018-09-06 17:10:57 -0300

    cria funções para passar páginas a partir de click nos btns

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import api from 'axios';
+import api from './services';
 
 export default class App extends Component {
 
@@ -9,26 +9,62 @@ export default class App extends Component {
     this.state = {
       numbers: null,
       page: 1,
-      perPage: 100
+      perPage: 100,
+      totalPages: null
     }
   }
 
   async componentDidMount() {
-    await this.getNumbers;
+    await this.getNumbers();
   }
 
-  async getNumbers() {
-    const numbers = await api.get(`/numbers?page=${this.state.page}&perPage=${this.state.petPage}`);
+  getNumbers = async () => {
+    const { perPage, page } = this.state;
+    const numbers = await api.get(`/numbers?page=${page}&perPage=${perPage}`);
 
-    console.log(numbers);
+    this.setState({
+      numbers: numbers.data.data,
+      totalPages: numbers.data.meta.totalPages
+    });
+  }
+
+  prevPage = async () => {
+    const { page } = this.state;
+
+    if (page > 1) {
+      this.setState({ page: page - 1 });
+    }
+
+    await this.getNumbers();
+  }
+
+  nextPage = async () => {
+    const { totalPages, page } = this.state;
+
+    if (page < totalPages) {
+      this.setState({ page: page + 1 });
+    }
+
+    await this.getNumbers();
   }
 
   render() {
     return (
       <div>
-        <div>
-          <h1>Hello daddy!</h1>
-        </div>
+        { this.state.numbers
+          ?
+          this.state.numbers.map((item, i) => {
+            return (
+              <ul key={i}>
+                <li>{item.number}</li>
+                <li>{item.cost}</li>
+              </ul>
+            )
+          })
+          : null
+        }
+        <button onClick={this.prevPage}>Prev</button>
+        <button onClick={this.nextPage}>Next</button>
       </div>
     );
   }
