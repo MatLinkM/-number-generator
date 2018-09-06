aeb6358c4834c4e3d12f26ebd176044dce194f72
Matheus Medeiros <matheuslinkm@gmail.com>
2018-09-06 11:48:51 -0300

    Adiciona axios como dependencia e cria func getNumbers

--- src/app.js
+++ src/app.js
@@ -1,6 +1,28 @@
 import React, { Component } from 'react';
 
+import api from 'axios';
+
 export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      numbers: null,
+      page: 1,
+      perPage: 100
+    }
+  }
+
+  async componentDidMount() {
+    await this.getNumbers;
+  }
+
+  async getNumbers() {
+    const numbers = await api.get(`/numbers?page=${this.state.page}&perPage=${this.state.petPage}`);
+
+    console.log(numbers);
+  }
+
   render() {
     return (
       <div>
